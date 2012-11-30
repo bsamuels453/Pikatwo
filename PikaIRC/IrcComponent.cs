14d5db833dc99c9db72a0ff6daf344f29e5dfd05
Ben Samuels <bsamuels453@gmail.com>
2012-11-30 13:42:25 -0800

    adding idisposable and handlemsg to interface

--- PikaIRC/IrcComponent.cs
+++ PikaIRC/IrcComponent.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace PikaIRC {
 // ReSharper disable InconsistentNaming
-    public interface IrcComponent {
+    public interface IrcComponent : IDisposable {
 // ReSharper restore InconsistentNaming
         bool Enabled { get; set; }
-
-
+        void HandleMsg(IrcMsg msg, StreamWriter writer);
     }
 }
