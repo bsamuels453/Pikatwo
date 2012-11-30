655a5bd7e15be04f3f85910621efb6b624c3364e
Ben Samuels <bsamuels453@gmail.com>
2012-11-30 14:55:58 -0800

    implementing some shared components

---
+++ PikaIRC/StandardComponents.cs
@@ -0,0 +1,124 @@
+﻿#region
+
+using System;
+using System.IO;
+
+#endregion
+
+namespace PikaIRC{
+    internal class JoinDefaultChannel : IrcComponent{
+        readonly string _channelToJoin;
+
+        public JoinDefaultChannel(string channelName){
+            _channelToJoin = channelName;
+            Enabled = true;
+        }
+
+        #region IrcComponent Members
+
+        public void Dispose(){}
+
+        public bool Enabled{get;set;}
+
+        public void Reset(){
+            Enabled = true;
+        }
+
+        public void HandleMsg(IrcMsg msg, IrcInstance.SendIrcCmd sendMethod) {
+            if (msg.Command == "376"){ //end of motd
+                sendMethod.Invoke(
+                    IrcCommand.Join,
+                    _channelToJoin
+                    );
+               
+                Enabled = false;//this component has no purpose after joining the default channel
+            }
+        }
+
+        #endregion
+    }
+
+    internal class IdentifyNick : IrcComponent{
+        readonly string _password;
+        readonly string _nick;
+
+        public IdentifyNick(string nick, string password){
+            Enabled = true;
+            _password = password;
+            _nick = nick;
+        }
+
+        public void Dispose(){
+        }
+
+        public bool Enabled {get;set;}
+
+        public void Reset(){
+            Enabled = true;
+        }
+
+        public void HandleMsg(IrcMsg msg, IrcInstance.SendIrcCmd sendMethod){
+            if (msg.Command == "376"){ //end of motd
+                sendMethod.Invoke(
+                    IrcCommand.Message,
+                    "Nickserv",
+                    string.Format("IDENTIFY {0} {1}", _nick, _password)
+                    );
+
+                Enabled = false; //this component has no purpose after joining the default channel
+            }
+        }
+    }
+
+    internal class NickCollisionHandler : IrcComponent{
+        readonly string _nick;
+        readonly string _password;
+
+        public NickCollisionHandler(string nick, string password){
+            _nick = nick;
+            _password = password;
+        }
+
+
+        public void Dispose(){
+
+        }
+
+        public bool Enabled {
+            get;
+            set;
+        }
+
+        public void Reset(){
+            Enabled = true;
+        }
+
+        public void HandleMsg(IrcMsg msg, IrcInstance.SendIrcCmd sendMethod) {
+            //someone else is already using this nick
+            if (msg.Command == "433"){
+                //change the nick
+                sendMethod.Invoke(
+                    IrcCommand.NickChange,
+                    _nick + DateTime.Now.Millisecond
+                    );
+
+                //ghost them if possible
+                if (_password != null){
+                    sendMethod.Invoke(
+                        IrcCommand.Message,
+                        "Nickserv",
+                        string.Format("GHOST {0} {1}", _nick, _password)
+                        );
+                }
+            }
+            //test to see if ghost worked
+            if (msg.Prefix.Contains("NickServ")
+                && msg.CommandParams.Contains("has been ghosted.")) {
+                    sendMethod.Invoke(
+                        IrcCommand.NickChange,
+                        _nick
+                        );
+            }
+        }
+    }
+}
