e68d59f0482a0f16bc7576e6716710b085385118
Ben Samuels <bsamuels453@gmail.com>
2012-11-30 12:59:52 -0800

    propagating renames and config updates

--- Pikatwo/Program.cs
+++ Pikatwo/Program.cs
@@ -12,7 +12,7 @@ namespace Pikatwo {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PikaForm());
         }
     }
 }
