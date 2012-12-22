71e87f0a0e36d57897da30a38d1ee2636c1552a4
Ben Samuels <bsamuels453@gmail.com>
2012-12-21 18:32:06 -0800

    adding temporary raw output logger

---
+++ Pikatwo/RawLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using IrcClient;
+
+namespace Pikatwo {
+    class RawLogger : IrcComponent{
+        readonly StreamWriter _sw;
+
+        public RawLogger(){
+            _sw = new StreamWriter("rawlog.txt");
+        }
+
+        public void Dispose(){
+            
+        }
+
+        public void Reset(){
+        }
+
+        public void HandleMsg(IrcMsg msg, IrcInstance.SendIrcCmd sendMethod){
+            _sw.WriteLine(msg.Prefix + " " + msg.Command + " " + msg.CommandParams + " " + msg.Trailing);
+            _sw.Flush();
+        }
+    }
+}
