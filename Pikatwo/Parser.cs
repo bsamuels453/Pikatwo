f565ba0917526e6c019b245aab66874a806297b9
Ben Samuels <bsamuels453@gmail.com>
2013-02-06 21:25:31 -0800

    partially implementing parser/nick blacklist

---
+++ Pikatwo/Parser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace Pikatwo {
+    static class Parser {
+        public static void ParseRaws(string inFileName, bool generateNicks=true){
+            //var sr = new StreamReader(inFileName);
+            GenNicks(inFileName);
+            var sentences = new List<string>();
+        }
+
+        static void GenNicks(string inFileName){
+            Regex regex;
+            using (var blacklistStrm = new StreamReader("blacklist.json")){
+                string regexCumulative = "";
+                var blackListStr = JsonConvert.DeserializeObject<string[]>(blacklistStrm.ReadToEnd());
+
+                foreach (var pattern in blackListStr){
+                    regexCumulative += pattern + "|";
+                }
+                regexCumulative = regexCumulative.Substring(0, regexCumulative.Count() - 1);
+                regex = new Regex(regexCumulative, RegexOptions.IgnoreCase);
+            }
+
+            var nicks = new List<string>();
+            using (var sr = new StreamReader(inFileName)){
+                string line;
+                while ((line = sr.ReadLine()) != null){
+                    int sepIdx = line.IndexOf(":");
+                    if (sepIdx <= 0)
+                        continue;
+                    string nick = line.Substring(0, sepIdx);
+                    if(!regex.IsMatch(nick) && !nicks.Contains(nick)){
+                        nicks.Add(nick);
+                    }
+                }
+            }
+
+            var orderedNicks = from nick in nicks
+                               where nick.Length >= 5
+                               orderby nick.Length descending
+                               select nick;
+
+            var sw = new StreamWriter("nicks.txt");
+            foreach (var nick in orderedNicks){
+                sw.WriteLine(nick);
+            }
+            sw.Flush();
+            sw.Close();
+            int f = 4;
+        }
+
+    }
+}
---
+++ RawParse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RawParse {
+    class Program {
+        /// <summary>
+        /// parses hexchat-generated log files into the pika-comaptible raw format
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) {
+            var sr = new StreamReader(args[0]);
+            var sw = new StreamWriter("output.txt");
+            sw.AutoFlush = true;
+
+            string input;
+            while ((input = sr.ReadLine()) != null){
+                if (input.Count() < 5)
+                    continue;
+                if (input.Contains("**** "))//block hexchat logging stuff
+                    continue;
+                if(input.Contains("*	"))//block hexchat logging stuff
+                    continue;
+                if (input.Contains("-NickServ-"))//block nickserv stuff
+                    continue;
+                int openingBracketIdx = input.IndexOf('<');
+                int closingBracketIdx = input.IndexOf('>');
+                if (openingBracketIdx == -1 || closingBracketIdx == -1)
+                    continue;
+                if (openingBracketIdx > closingBracketIdx)
+                    continue;
+                string name = input.Substring(openingBracketIdx+1, closingBracketIdx - openingBracketIdx-1);
+                string content = input.Substring(closingBracketIdx + 2);
+                sw.WriteLine(name + ":" + content);
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
