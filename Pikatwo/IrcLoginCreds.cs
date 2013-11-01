a8ebde4b5cf429d680b277cc26db3e0a1ff73d84
Ben Samuels <bsamuels453@gmail.com>
2013-10-31 19:17:26 -0700

    adding irclogincreds

---
+++ Pikatwo/IrcLoginCreds.cs
@@ -0,0 +1,10 @@
+﻿namespace Pikatwo{
+    internal class IrcLoginCreds{
+        public string Nick;
+        public string Password;
+        public int Port;
+        public string RealName;
+        public string Server;
+        public string UserName;
+    }
+}
