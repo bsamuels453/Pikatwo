f90099303a1cb2e97dc29f111a408eb638b561a4
Ben Samuels <bsamuels453@gmail.com>
2013-10-31 19:17:37 -0700

    adding reconnector component

---
+++ Pikatwo/Reconnector.cs
@@ -0,0 +1,30 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace Pikatwo{
+    internal class Reconnector : IrcComponent{
+        ClientInterface _ircClient;
+
+        #region IrcComponent Members
+
+        public ClientInterface IrcClient{
+            get { return _ircClient; }
+            set{
+                _ircClient = value;
+                _ircClient.OnDisconnect += OnDisconnect;
+            }
+        }
+
+        public void Update(long secsSinceStart){
+        }
+
+        #endregion
+
+        void OnDisconnect(object sender, EventArgs eventArgs){
+            _ircClient.Connect();
+        }
+    }
+}
