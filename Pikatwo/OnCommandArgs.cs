c8274308f7a2df773d460455265dc50e7f0bceb4
Ben Samuels <bsamuels453@gmail.com>
2013-11-04 15:21:17 -0800

    adding oncommandargs

---
+++ Pikatwo/OnCommandArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace Pikatwo{
+    internal class OnCommandArgs{
+        public readonly AuthLevel AuthLevel;
+        public readonly string From;
+        public readonly string Host;
+        public readonly string Ident;
+        public readonly string Message;
+        public readonly string Nick;
+
+        public OnCommandArgs(AuthLevel authLevel, string @from, string host, string ident, string message, string nick){
+            AuthLevel = authLevel;
+            From = @from;
+            Host = host;
+            Ident = ident;
+            Message = message;
+            Nick = nick;
+        }
+    }
+}
