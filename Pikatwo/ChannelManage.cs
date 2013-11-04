887b1d334ae49eb6cb9e5952b49b8641687f3622
Ben Samuels <bsamuels453@gmail.com>
2013-11-04 14:53:44 -0800

    adding channel manager component

---
+++ Pikatwo/ChannelManage.cs
@@ -0,0 +1,47 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using Meebey.SmartIrc4net;
+
+#endregion
+
+namespace Pikatwo{
+    internal class ChannelManage : IrcComponent{
+        readonly List<string> _channels;
+        ClientInterface _ircInterface;
+
+        public ChannelManage(string[] channels){
+            _channels = new List<string>();
+            _channels.AddRange(channels);
+        }
+
+        #region IrcComponent Members
+
+        public ClientInterface IrcInterface{
+            get { return _ircInterface; }
+            set{
+                _ircInterface = value;
+                _ircInterface.Client.OnRegistered += OnRegistered;
+                _ircInterface.Client.OnKick += OnKick;
+            }
+        }
+
+        public void Update(long secsSinceStart){
+        }
+
+        #endregion
+
+        void OnRegistered(object sender, EventArgs eventArgs){
+            foreach (var channel in _channels){
+                _ircInterface.Client.RfcJoin(channel);
+            }
+        }
+
+        void OnKick(object sender, KickEventArgs kickEventArgs){
+            if (kickEventArgs.Whom.Equals(_ircInterface.Nick)){
+                _ircInterface.Client.RfcJoin(kickEventArgs.Channel);
+            }
+        }
+    }
+}
