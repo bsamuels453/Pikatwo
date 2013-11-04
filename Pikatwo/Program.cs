7d1d6dcd4a44f74d9793de3d3364822e0c4dfd4d
Ben Samuels <bsamuels453@gmail.com>
2013-11-04 15:40:04 -0800

    implementing program init. code

---
+++ Pikatwo/Program.cs
@@ -0,0 +1,33 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+#endregion
+
+namespace Pikatwo{
+    internal static class Program{
+        const string _configFile = "config.json";
+
+        public static void Main(string[] args){
+            var sr = new StreamReader(_configFile);
+            var config = JObject.Parse(sr.ReadToEnd());
+
+            var creds = new IrcLoginCreds();
+            creds.Nick = config["Nick"].ToObject<string>();
+            creds.Password = config["Password"].ToObject<string>();
+            creds.Port = config["Port"].ToObject<int>();
+            creds.Server = config["Server"].ToObject<string>();
+            creds.RealName = config["RealName"].ToObject<string>();
+            creds.UserName = config["UserName"].ToObject<string>();
+
+            var components = new List<IrcComponent>();
+            var channels = config["Channels"].ToObject<string[]>();
+            components.Add(new ChannelManage(channels));
+
+            var client = new ClientInterface(creds, components);
+            client.Run();
+        }
+    }
+}
