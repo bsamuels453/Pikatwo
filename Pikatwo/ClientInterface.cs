34a8012b94d29418890eb00a326c97437f44e81a
Ben Samuels <bsamuels453@gmail.com>
2013-11-04 15:39:54 -0800

    implementing clientinterface

---
+++ Pikatwo/ClientInterface.cs
@@ -0,0 +1,112 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Meebey.SmartIrc4net;
+
+#endregion
+
+namespace Pikatwo{
+    internal delegate void OnIrcCommand(OnCommandArgs args);
+
+    /// <summary>
+    /// exists to hide most of the ircclient stuff we dont want to expose to irccomponents
+    /// </summary>
+    internal class ClientInterface{
+        const char _commandChar = '.';
+        readonly Authenticator _authenticator;
+
+        readonly List<IrcComponent> _components;
+        readonly StreamWriter _debugWriter;
+        readonly IrcLoginCreds _loginCreds;
+        readonly StreamWriter _rawWriter;
+
+        public ClientInterface(IrcLoginCreds loginCreds, List<IrcComponent> auxComponents){
+            Client = new IrcClient();
+            Client.Encoding = Encoding.UTF8;
+            Client.SendDelay = 200;
+            Client.ActiveChannelSyncing = true;
+            _loginCreds = loginCreds;
+
+            _authenticator = new Authenticator();
+
+            _components = auxComponents;
+            _components.Add(_authenticator);
+
+            foreach (var component in _components){
+                component.IrcInterface = this;
+            }
+
+            Client.OnChannelMessage += HandleCommands;
+            Client.OnQueryMessage += HandleCommands;
+            Client.OnRawMessage += ClientOnOnRawMessage;
+
+            _debugWriter = new StreamWriter("debugOut.txt", true);
+            _rawWriter = new StreamWriter("rawOut.txt", true);
+        }
+
+
+        public IrcClient Client { get; private set; }
+
+        public string Nick{
+            get { return Client.Nickname; }
+        }
+
+        void ClientOnOnRawMessage(object sender, IrcEventArgs ircEventArgs){
+            var unixTime = (long) ((DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds);
+            _rawWriter.WriteLine(unixTime + " " + ircEventArgs.Data.RawMessage);
+            _rawWriter.Flush();
+        }
+
+        void HandleCommands(object sender, IrcEventArgs ircEventArgs){
+            if (ircEventArgs.Data.Message[0] == _commandChar){
+                var authLevel = _authenticator.GetUserAuthLevel(ircEventArgs.Data.Host);
+                var onCommand = new OnCommandArgs
+                    (
+                    authLevel,
+                    ircEventArgs.Data.From,
+                    ircEventArgs.Data.Host,
+                    ircEventArgs.Data.Ident,
+                    ircEventArgs.Data.Message,
+                    ircEventArgs.Data.Nick
+                    );
+
+                DebugLog("Command; Nick:" + ircEventArgs.Data.Nick + " Authlevel:" + authLevel.ToString() + " Trailing:" + ircEventArgs.Data.Message);
+                if (OnIrcCommand != null){
+                    OnIrcCommand.Invoke(onCommand);
+                }
+            }
+        }
+
+        public event OnIrcCommand OnIrcCommand;
+
+
+        public void Run(){
+            Connect();
+            while (true){
+                Client.ReadLine(true);
+            }
+        }
+
+        public void DebugLog(string text){
+            var unixTime = (long) ((DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds);
+            _debugWriter.WriteLine(unixTime + " " + text);
+            _debugWriter.Flush();
+        }
+
+        public void Connect(){
+            bool connectionSuccessful = false;
+            do{
+                try{
+                    Client.Connect(_loginCreds.Server, _loginCreds.Port);
+                    Client.Login(_loginCreds.Nick, _loginCreds.RealName, 0, _loginCreds.UserName, _loginCreds.Password);
+                    connectionSuccessful = true;
+                }
+                catch (ConnectionException e){
+                }
+            } while (!connectionSuccessful);
+        }
+    }
+}
